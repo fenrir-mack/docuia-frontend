3640de5704a33c8c72e87c377a5228672a13873a
fenrir-mack <10436455@mackenzista.com.br>
2026-05-22 19:33:18 -0300

    conectando com o diagrama

--- main.py
+++ main.py
@@ -23,14 +23,17 @@ async def api_config_js():
     auth_url = os.getenv("AUTH_API_URL", "http://localhost:8001")
     empresas_url = os.getenv("EMPRESAS_API_URL", "http://localhost:8002")
     projetos_url = os.getenv("PROJETOS_API_URL", "http://localhost:8003")
-    amigos_url = os.getenv("AMIGOS_FRONTEND_URL", "http://localhost:5000")
-    
+    Upload_url = os.getenv("UPLOAD_FRONTEND_URL", "http://localhost:5000")
+    Diagramas_url = os.getenv("Diagramas_FRONTEND_URL", "http://localhost:5000")
+
+
     js_content = f"""
     window.API_CONFIG = {{
         auth: "{auth_url}",
         empresas: "{empresas_url}",
         projetos: "{projetos_url}",
-        amigos: "{amigos_url}"
+        amigos: "{amigos_url}",
+        diagramas: "{diagramas_url}"
     }};
     """
     return Response(content=js_content, media_type="application/javascript")
